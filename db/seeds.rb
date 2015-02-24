5c67ae74fd6cdd69ce49570a87a038df66826007
Sichacvah <sichirc@gmail.com>
2015-02-24 06:37:32 +0500

    fourth task

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
-# Создадим записи в базе данных для проверки,
-# восклицательный знак после create стоит для вызова исключения.
 # encoding: utf-8
 require 'open-uri'
 require 'nokogiri'
 
+URL = "http://masterrussian.com/vocabulary/most_common_words.htm"
+
 class WordsDownloader
-  def initialize(url, original_text_col=2, translated_text_col=3)
+  def initialize(url, original_text_col = 2, translated_text_col = 3)
     @url = url
     @original_text_col = original_text_col
     @translated_text_col = translated_text_col
   end
 
   def get_words(&closure)
-    get_rows() if @rows == nil
+    get_rows if @rows == nil
     words = []
-    closure = lambda { |row| return row.css('td') } || lambda { closure }
+    closure = lambda { |row| return row.css("td") } || lambda { closure }
     @rows.each do |row|
       cols = closure.call(row)
       words << [cols[@original_text_col].text, cols[@translated_text_col].text]
@@ -22,27 +22,25 @@ class WordsDownloader
     return words
   end
 
-
-
   def get_rows(&closure)
     doc = Nokogiri::HTML(open(@url))
-    closure = lambda { return doc.css('table.topwords').css('tr')[1..-1] } || lambda { closure }
+    closure = lambda { return doc.css("table.topwords").css("tr")[1..-1] } || lambda { closure }
     @rows = closure.call
   end
 end
 
 class CardCreator < WordsDownloader
   def create
-    words = get_words()
+    words = get_words
     words.each do |original_text, translated_text|
-      Card.create!({original_text: original_text,
-                    translated_text: translated_text})
+      Card.create!({ original_text: original_text,
+                     translated_text: translated_text })
     end
   end
 end
 
-
-cards_creator = CardCreator.new("http://masterrussian.com/vocabulary/most_common_words.htm")
+Card.delete_all
+cards_creator = CardCreator.new(URL)
 cards_creator.create
 puts "created #{Card.count} cards"
 
