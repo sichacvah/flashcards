54ad0e69d1aaf24fde75e2aebb08eb7bb9dcc910
Sichacvah <sichirc@gmail.com>
2015-02-24 19:02:00 +0500

    fourth-task

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,63 +2,24 @@
 require "open-uri"
 require "nokogiri"
 
-class WordsDownloader
-  def initialize(url, original_text_col = 2, translated_text_col = 3,
-  table_selector = "table.topwords")
-    @url = url
-    @original_text_col = original_text_col
-    @translated_text_col = translated_text_col
-    @table_selector = table_selector
-  end
-
-  def get_words(&closure)
-    get_rows if @rows == nil
-    words = []
-    closure =
-    if closure.nil?
-      lambda { |row| return row.css("td") }
-    else
-      # используется если требуемая информация находится не в табличной форме
-      lambda { closure }
-    end
-    @rows.each do |row|
-      cols = closure.call(row)
-      words << [cols[@original_text_col].text, cols[@translated_text_col].text]
-    end
-    words
-  end
-
-  def get_rows(&closure)
-    doc = Nokogiri::HTML(open(@url))
-    closure =
-    if closure.nil?
-      lambda { return doc.css(@table_selector).css("tr")[1..-1] }
-    else
-      # используется если требуемая информация находится не в табличной форме
-      lambda { closure }
-    end
-    @rows = closure.call
-  end
-end
-
-class CardCreator < WordsDownloader
-  def create
-    words = get_words
-    words.each do |original_text, translated_text|
-      Card.create!(
-                  original_text: original_text,
-                  translated_text: translated_text
-                  )
-    end
-  end
-end
 
 Card.delete_all
 
 URL = "http://masterrussian.com/vocabulary/most_common_words.htm"
+ORIGINAL_COL = 2
+TRANSLATED_COL = 3
 
-cards_creator = CardCreator.new(URL)
-cards_creator.create
+document = Nokogiri::HTML(open(URL))
+rows = document.css("table.topwords").css('tr')[1..-1]
+
+rows.each do |row|
+  cols = row.css("td")
+  original_text = cols[ORIGINAL_COL].text
+  translated_text = cols[TRANSLATED_COL].text
+  Card.create! original_text: original_text, translated_text: translated_text
+end
 
 logger = Logger.new(STDOUT)
 logger.info "created #{Card.count} cards"
+
+
