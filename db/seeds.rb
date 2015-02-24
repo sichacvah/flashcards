036a1baffca5c9f517325b1d011fa2e0f9f5cd79
Sichacvah <sichirc@gmail.com>
2015-02-24 06:29:06 +0500

    fourth task

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,48 @@
 # Создадим записи в базе данных для проверки,
 # восклицательный знак после create стоит для вызова исключения.
+# encoding: utf-8
+require 'open-uri'
+require 'nokogiri'
 
-Card.create!([
-  { original_text: "Привет. Как дела?",
-    translated_text: "Hello. How are you?",
-    review_date: Date.current },
+class WordsDownloader
+  def initialize(url, original_text_col=2, translated_text_col=3)
+    @url = url
+    @original_text_col = original_text_col
+    @translated_text_col = translated_text_col
+  end
 
-  { original_text: "Откуда ты?",
-    translated_text: "where are you from?",
-    review_date: Date.yesterday },
+  def get_words(&closure)
+    get_rows() if @rows == nil
+    words = []
+    closure = lambda { |row| return row.css('td') } || lambda { closure }
+    @rows.each do |row|
+      cols = closure.call(row)
+      words << [cols[@original_text_col].text, cols[@translated_text_col].text]
+    end
+    return words
+  end
 
-  { original_text: "Привет. Как дела?",
-    translated_text: "Hello. How are you?",
-    review_date: Date.new(2015, 02, 12) }
-            ])
+
+
+  def get_rows(&closure)
+    doc = Nokogiri::HTML(open(@url))
+    closure = lambda { return doc.css('table.topwords').css('tr')[1..-1] } || lambda { closure }
+    @rows = closure.call
+  end
+end
+
+class CardCreator < WordsDownloader
+  def create
+    words = get_words()
+    words.each do |original_text, translated_text|
+      Card.create!({original_text: original_text,
+                    translated_text: translated_text})
+    end
+  end
+end
+
+
+cards_creator = CardCreator.new("http://masterrussian.com/vocabulary/most_common_words.htm")
+cards_creator.create
+puts "created #{Card.count} cards"
+
