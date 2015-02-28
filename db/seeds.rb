48d222b3eae0b1083c15ee6ce7cca20bcb8fcc57
Sichacvah <sichirc@gmail.com>
2015-02-28 15:14:56 +0500

    work on the error

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 require "open-uri"
 require "nokogiri"
 
-Card.delete_all
+User.delete_all
 
+user = User(email: "email@email.com", password: "password")
 URL = "http://masterrussian.com/vocabulary/most_common_words.htm"
 ORIGINAL_COL = 2
 TRANSLATED_COL = 3
@@ -14,7 +15,7 @@ rows.each do |row|
   cols = row.css("td")
   original_text = cols[ORIGINAL_COL].text
   translated_text = cols[TRANSLATED_COL].text
-  Card.create! original_text: original_text, translated_text: translated_text
+  user.cards.create! original_text: original_text, translated_text: translated_text
 end
 
 logger = Logger.new(STDOUT)
