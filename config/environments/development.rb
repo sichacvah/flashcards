c254c89d1f326bae74e1ef9a03b5799c21563181
Sichacvah <sichirc@gmail.com>
2015-03-02 22:10:00 +0500

    work on the error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: "sichacvah",
+      bucket: ENV["AWS_BUCKET"],
       access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       secret_access_key: ENV["AWS_SECRET_KEY_ID"]
     }
