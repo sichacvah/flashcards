23b6ab926bd599093e668a7bd06615281602c52f
Sergez <sichacvah@gmail.com>
2015-03-09 19:05:06 +0500

    Update card_mailer.rb

--- app/mailers/card_mailer.rb
+++ app/mailers/card_mailer.rb
@@ -4,7 +4,7 @@ class CardMailer < ActionMailer::Base
   def pending_cards_notification(user, cards_for_review_count)
     @user = user
     @cards_for_review_count = cards_for_review_count
-    @url = ENV['SELF_URL']
+    @url = root_path
     mail(to: @user.email, subject: "У вас есть не проверенные карточки.")
   end
 end
