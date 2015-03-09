67a1a30e23bf34862d2520941b185996594576a5
Sergez <sichacvah@gmail.com>
2015-03-09 18:11:31 +0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,12 +88,12 @@ Rails.application.configure do
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      domain: "gmail.com",
-      authentication: "plain",
-      user_name: ENV["GMAIL_LOGIN"],
-      password: ENV["GMAIL_PASSWORD"],
-      enable_starttls_auto: true
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    user_name: ENV["GMAIL_LOGIN"],
+    password: ENV["GMAIL_PASSWORD"],
+    enable_starttls_auto: true
   }
 end
