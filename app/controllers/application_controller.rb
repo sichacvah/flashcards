bac5faa7a1ea582d891c42d2b93e43cb3c746fe4
Sergez <sichacvah@gmail.com>
2015-03-10 08:26:37 +0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,16 +15,15 @@ class ApplicationController < ActionController::Base
   protected
 
   def set_locale
-    locale = if current_user
-               current_user.locale
-             elsif params[:locale]
-               session[:locale] = params[:locale]
-             elsif session[:locale]
-               session[:locale]
-             else
-               http_accept_language.
-                 compatible_language_from(I18n.available_locales)
-             end
+    locale =  if current_user
+                current_user.locale
+              elsif params[:locale]
+                session[:locale] = params[:locale]
+              elsif session[:locale]
+                session[:locale]
+              else
+                http_accept_language.compatible_language_from(I18n.available_locales)
+              end
     if locale && I18n.available_locales.include?(locale.to_sym)
       session[:locale] = I18n.locale = locale.to_sym
     end
