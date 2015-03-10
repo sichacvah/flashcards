0c62764c87f4a97a9102ab67a95c22cf00c713cc
Sergez <sichacvah@gmail.com>
2015-03-10 10:11:25 +0500

    Update decks_spec.rb

--- spec/features/decks_spec.rb
+++ spec/features/decks_spec.rb
@@ -3,7 +3,7 @@ require "support/login"
 require "database_cleaner"
 
 DatabaseCleaner.strategy = :truncation
-Capybara.default_driver = :selenium
+Capybara.default_driver = :rack_driver
 
 describe "the deck card process" do
   before do
