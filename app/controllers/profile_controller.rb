13044a9139f5be0bf27bd79b3a66aa8de4a37c52
Sergez <sichacvah@gmail.com>
2015-03-10 08:27:35 +0500

    Update profile_controller.rb

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -13,6 +13,7 @@ class ProfileController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :locale)
+    params.require(:user).permit(:email, :password, :password_confirmation,
+                                 :locale)
   end
 end
