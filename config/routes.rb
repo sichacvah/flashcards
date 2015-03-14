e160afc753dd79bd407390834bdb98470357cb59
Sichacvah <sichirc@gmail.com>
2015-03-14 22:16:29 +0500

    use namespaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root "home#index"
-  scope :home , module: 'home' do
+  scope :home, module: "home" do
     resources :registration, only: [:new, :create]
     post "oauth/callback" => "oauths#callback"
     get "oauth/callback" => "oauths#callback"
     get "oauth/:provider" => "oauths#oauth", as: :auth_at_provider
   end
 
-  scope :dashboard, module: 'dashboard' do
+  scope :dashboard, module: "dashboard" do
     get "review" => "review#index"
     get "login" => "sessions#new"
     put "review_card" => "review#review_card"
