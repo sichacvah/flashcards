2b211fdb199bad92ab9f92738550ee92c50005d8
Sichacvah <sichirc@gmail.com>
2015-03-15 17:06:18 +0500

    split login in two methods

--- app/controllers/dashboard/review_controller.rb
+++ app/controllers/dashboard/review_controller.rb
@@ -17,24 +17,29 @@ class Dashboard::ReviewController < ApplicationController
   end
 
   def review_card
+
     @card = Card.find(review_params[:card_id])
     check_result = @card.check_translation review_params[:user_input], review_params[:time_to_answer]
     respond_to do |format|
-      if check_result == :success
-        flash[:success] = t(:success)
-      elsif check_result == :incomplete_match
-        flash[:warning] = t(:missprint, user_input: review_params[:user_input],
-                                        translate: @card.translated_text,
-                                        original: @card.original_text)
-      elsif check_result == :failed
-        flash[:danger] = t(:fail)
-      end
+      set_flash(check_result)
       format.html { redirect_to review_path }
       format.json { render json: { message: flash }.to_json }
     end
   end
 
   private
+
+  def set_flash(check_result)
+    if check_result == :success
+      flash[:success] = t(:success)
+    elsif check_result == :incomplete_match
+      flash[:warning] = t(:missprint, user_input: review_params[:user_input],
+                                      translate: @card.translated_text,
+                                      original: @card.original_text)
+    elsif check_result == :failed
+      flash[:danger] = t(:fail)
+    end
+  end
 
   def review_params
     params.permit(:card_id, :user_input, :time_to_answer)
